02fc02181e6e88c66ef5877b72475baa67558eda
Takbir Zaman Bhuiyan <takbirzamanbhuiyan@gmail.com>
2026-05-21 00:12:25 +0600

    lst commit

--- rag.py
+++ rag.py
@@ -4,7 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_anthropic import ChatAnthropic
